b474583b93355d9270a324c608d528d996d7aa2f
sepo21 <makefirst1@gmail.com>
2020-06-20 11:22:57 +0900

    add pug

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -6,9 +6,9 @@ import { join, login, logout } from "../controllers/userController";
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, search);
-globalRouter.get(routes.login, join);
-globalRouter.get(routes.logout, login);
-globalRouter.get(routes.search, logout); 
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.search, search); 
 
 export default globalRouter;
